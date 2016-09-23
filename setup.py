928b2b24e320cefc75fcd6890ca5ded2478a7f69
balazs.saros <balazs.saros@gmail.com>
2016-09-23 11:33:41 +0200

    updated setup infos

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(
     name='bootable-usb',
     version='0.2',
-    scripts=['bootable-usb']
+    scripts=['bootable-usb'],
+    url='https://www.github.com/balzss/bootable-usb',
+    license='GPLv3'
 )
