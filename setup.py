0cc9468b66510a71b980246b3a5b2e9dbd57a8d1
balazs.saros <balazs.saros@gmail.com>
2016-09-24 19:03:52 +0200

    bugfixes for 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bootable-usb',
-    version='0.3',
+    version='0.3.1',
     scripts=['bootable-usb'],
     url='https://www.github.com/balzss/bootable-usb',
     license='GPLv3',
