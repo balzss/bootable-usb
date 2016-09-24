bca3e2bf5f8820b0c6017e9b3537ff225dbd80bc
balazs.saros <balazs.saros@gmail.com>
2016-09-24 18:33:00 +0200

    finalized changes for 0.3 release

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup
 
 setup(
     name='bootable-usb',
-    version='0.2',
+    version='0.3',
     scripts=['bootable-usb'],
     url='https://www.github.com/balzss/bootable-usb',
-    license='GPLv3'
+    license='GPLv3',
+    description='Wrapper utility around *dd* to put iso images on usb-s fast and effortless',
+    author='balzss',
+    author_email='balazs.saros@gmail.com',
+    keywords='dd usb iso boot'
 )
